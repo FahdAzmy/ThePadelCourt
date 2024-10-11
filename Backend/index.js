bace1a087ac5fe54173ea2895afb53efb0b610c5
MostafaRabie <mostaga.kxkk34@gmail.com>
2024-10-12 02:36:20 +0300

    done

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN || "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
 );
