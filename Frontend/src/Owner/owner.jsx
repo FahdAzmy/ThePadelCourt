fe8e5c86ebdf0bceac130564fdaff6e25693c8b7
FahdAzmy <azmyfahd66@gmail.com>
2024-10-20 12:31:41 +0200

    edit to deploy

--- Frontend/src/Owner/owner.jsx
+++ Frontend/src/Owner/owner.jsx
@@ -1,12 +1,12 @@
-import Proj from "./proj"
-const Owner =()=>{
-
-    return <>
-        <div className="">
-            <Proj/>
-        </div>
-    
+import { Proj } from "./proj";
+const Owner = () => {
+  return (
+    <>
+      <div className="">
+        <Proj />
+      </div>
     </>
-}
+  );
+};
 
-export default Owner ;
+export default Owner;
